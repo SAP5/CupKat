b3d34d1ede3fc24581e7e281b9836cbfb38a0a24
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-10-16 16:23:28 -0300

    validação de senha forte e mensagem para campos vazios

--- src/main/java/com/example/cupcat/model/Usuario.java
+++ src/main/java/com/example/cupcat/model/Usuario.java
@@ -7,17 +7,19 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Usuario extends Model{
-    @NotEmpty
+    @NotEmpty(message = "o nome não pode ser vazio")
     private String nome;
-    @NotEmpty
+    @NotEmpty(message = "o email não pode ser vazio")
     @Email
     private String email;
-    @NotEmpty
+    @NotEmpty(message = "A senha não pode estar vazia")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\\1)){8,}$", message = "A senha é muito fraca")
     private String senha;
 }
