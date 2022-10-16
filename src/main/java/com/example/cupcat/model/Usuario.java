f986c3f684d330db60205b3b1ea194282e66024c
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-10-16 16:15:42 -0300

    validação de nome, senha e email não podem vir vazios e validação se o formato do email está valido

--- src/main/java/com/example/cupcat/model/Usuario.java
+++ src/main/java/com/example/cupcat/model/Usuario.java
@@ -5,12 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Usuario extends Model{
+    @NotEmpty
     private String nome;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
     private String senha;
 }
