7ba8d01b77cee81eb2a4132edea6606d56727849
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-10-24 22:24:10 -0300

    atualização dos repositórios, deixando eles da maneira mais básica possível

--- src/main/java/com/example/cupcat/repository/FuncionarioRepo.java
+++ src/main/java/com/example/cupcat/repository/FuncionarioRepo.java
@@ -1,49 +1,6 @@
 package com.example.cupcat.repository;
 
-import com.example.cupcat.model.Cliente;
 import com.example.cupcat.model.Funcionario;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.beans.ConstructorProperties;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-@Repository
-public class FuncionarioRepo {
-    private final String linkFile = "src/main/resources/funcionario.json";
-    private ObjectMapper mapper = new ObjectMapper();
-
-    public List<Funcionario> getAll() {
-        try {
-            return Arrays.asList(mapper.readValue(new File(linkFile), Funcionario[].class));
-        } catch (Exception ex) {
-            System.out.println("Erro ao ler o arquivo");
-        }
-        return null;
-    }
-
-    public boolean saveFuncionario(Funcionario funcionario) {
-        if (funcionarioAlreadyExists(funcionario))
-            return false;
-        List<Funcionario> funcionarios = new ArrayList<>(getAll());
-        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
-        try {
-            funcionarios.add(funcionario);
-            writer.writeValue(new File(linkFile), funcionarios);
-            return true;
-        } catch (Exception ex) {
-            System.out.println("Erro ao salvar os dados!");
-        }
-        return false;
-    }
-
-    public boolean funcionarioAlreadyExists(Funcionario funcionario) {
-        return getAll().contains(funcionario) ||
-                getAll().get(funcionario.getId() - 1).getEmail().equals(funcionario.getEmail());
-    }
-}
+public interface FuncionarioRepo extends JpaRepository<Funcionario, Integer> {}
